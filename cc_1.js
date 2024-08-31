43e151cdbc0f81cfd2db5f0925044a25a33e0f66
coryrmrz <coryramirez2018@gmail.com>
2024-08-30 20:01:45 -0400

    Task1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variable and Data Types
+
+let customerName = "Kimberly Ramirez";
+const customerID = 26;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
